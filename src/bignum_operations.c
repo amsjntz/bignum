011f654e5624c6576b9a9563dbcdf327e7f29c1e
Amos <amsja.mull@gmail.com>
2024-01-07 12:06:28 +0100

    worked on multiplication - not finished yet

--- src/bignum_operations.c
+++ src/bignum_operations.c
@@ -2,6 +2,8 @@
 #include "bignum_helpers.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 // treats a and b as always positive, will always return a positive value
 bignum_t* unsigned_add(const bignum_t* a, const bignum_t* b) {
@@ -102,11 +104,34 @@ bignum_t* bignum_multiply(const bignum_t* a, const bignum_t* b) {
 	int bmaxpow = get_maximal_power(b);
 	int bminpow = get_minimal_power(b);
 
+	struct powered_number {
+		unsigned int value;
+		int power;
+	};
+
+	unsigned int num_idx = 0;
+	struct powered_number numbers[get_true_length(a) * get_true_length(b)];
 
 	for (int bpower = bmaxpow; bpower >= bminpow; bpower--) {
-		for (int apower = aminpow; apower < amaxpow; apower++) {
+		for (int apower = aminpow; apower <= amaxpow; apower++) {
 			unsigned int product = get_digit_at(a, apower) * get_digit_at(b, bpower);
+			numbers[num_idx++] = (struct powered_number) {
+				.value = product,
+				.power = apower + bpower
+			};
 		}
 	}
+
+	unsigned int result_length = get_true_length(a) + get_true_length(b);
+	char result[result_length];
+	memset(result, '0', result_length);
+	result[result_length - 1] = '\0';
+	int minpow = MIN(aminpow, bminpow);
+	if (minpow < 0) {
+		result[result_length - 2 + minpow] = '.';
+	}
+
+	printf("%s\n", result);
+
 	return bignum_create_from_string("0");
 }
