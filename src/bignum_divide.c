4e9c4bdcaf1b33507236549759fcb82b4530d902
Amos <amsja.mull@gmail.com>
2024-01-25 17:14:15 +0100

    made division a lot faster

--- src/bignum_divide.c
+++ src/bignum_divide.c
@@ -2,64 +2,101 @@
 #include "bignum_helpers.h"
 
 #include <stdlib.h>
+#include <stdbool.h>
 
-// this works on the assumption that when a / b = result, b * result = a.
-// it basically brute forces its way to the result by starting at 5 for every digit,
-// and then adjusting it until we got closer to the correct result.
-// very memory intensive and slow. FIXME
-bignum_t* unsigned_divide(const bignum_t* a, const bignum_t* b, unsigned int digits) {
-	bignum_t* result = bignum_create_from_string("5");
-	bignum_t* nudge = bignum_create_from_string("1");
-	bignum_t* nudgefactor = bignum_create_from_string("0.1");
+typedef struct {
+	bignum_t* lower;
+	bignum_t* upper;
+} narrowed_result_t;
 
-	unsigned int generated_digits = 0;
+typedef struct {
+	const bignum_t* a;
+	const bignum_t* b;
+	const bignum_t* max_difference;
+} bignum_division_t;
 
-	do {
-		bignum_comparison_t lastcomp = BIGNUM_LT;
-		while (true) {
-			bignum_t* check = bignum_multiply(result, b);
-			bignum_comparison_t comp = bignum_unsigned_compare(check, a);
-			bignum_cleanup(check);
+bool precise_enough(narrowed_result_t* result, const bignum_t* max_difference) {
+	if (result->lower->whole_digits != result->upper->whole_digits) {
+		return false;
+	}
 
-			// last tried digit was too small, new one is too big 
-			// -> result must be in between, so the previous attempt was correct
-			if (lastcomp == BIGNUM_GT && comp == BIGNUM_LT) {
+	bignum_t* difference = bignum_subtract(result->lower, result->upper);
+	bignum_comparison_t cmp = bignum_unsigned_compare(difference, max_difference);
+	bignum_cleanup(difference);
+	return cmp != BIGNUM_GT;
+}
+
+void narrow_iterative(narrowed_result_t* bounds, bignum_division_t* division) {
+	bignum_t* half_factor = bignum_create_from_string("0.5");
+
+	while (!precise_enough(bounds, division->max_difference)) {
+		bignum_t* sum = bignum_add(bounds->lower, bounds->upper);
+		bignum_t* center = bignum_multiply(sum, half_factor);
+		bignum_cleanup(sum);
+
+		bignum_t* check = bignum_multiply(center, division->b);
+		bignum_comparison_t comparison = bignum_unsigned_compare(check, division->a);
+		bignum_cleanup(check);
+
+		if (comparison == BIGNUM_LT) {
+			bignum_cleanup(bounds->lower);
+			bounds->lower = center;
+		} else {
+			bignum_cleanup(bounds->upper);
+			bounds->upper = center;
+			// center contains correct result -> exit loop
+			if (comparison == BIGNUM_EQ) {
 				break;
 			}
-
-			bignum_t* newres;
-			switch (comp) {
-				case BIGNUM_EQ:
-					goto exit; // result * b = a -> we can exit early!
-				case BIGNUM_LT:
-					// result * b < a -> result too small
-					newres = bignum_add(result, nudge);
-					break;
-				case BIGNUM_GT:
-					// result * b > a -> result too big
-					newres = bignum_subtract(result, nudge); 
-					break;
-			}
-			bignum_cleanup(result);
-			result = newres;
-
-			lastcomp = comp;
 		}
+	}
 
-		bignum_t* newnudge = bignum_multiply(nudge, nudgefactor);
-		bignum_cleanup(nudge);
-		nudge = newnudge;
+	bignum_cleanup(half_factor);
+}
 
-		if (get_minimal_power(result) < 0) {
-			generated_digits++;
+// returns pow(10, power)
+bignum_t* create_bignum_by_power(int power) {
+	if (power >= 0) {
+		char str[power + 2];
+		for (int i = 1; i < power + 1; i++) {
+			str[i] = '0';
 		}
+		str[0] = '1';
+		str[power + 1] = '\0';
+		return bignum_create_from_string(str);
+	} else {
+		char str[-power + 3];
+		for (int i = 0; i < -power + 1; i++) {
+			str[i] = '0';
+		}
+		str[1] = '.';
+		str[-power + 1] = '1';
+		str[-power + 2] = '\0';
+		return bignum_create_from_string(str);
+	}
+}
 
-	} while (generated_digits < digits);
+bignum_t* unsigned_divide(const bignum_t* a, const bignum_t* b, unsigned int digits) {
+	narrowed_result_t bounds;
+	int upper_power = MAX(get_maximal_power(a) - get_minimal_power(b),
+			get_maximal_power(b) - get_minimal_power(a));
+	bounds.upper = create_bignum_by_power(upper_power);
+	bounds.lower = bignum_create_from_string("0");
 
-exit:
-	bignum_cleanup(nudge);
-	bignum_cleanup(nudgefactor);
-	return result;
+	bignum_division_t division;
+	division.a = a;
+	division.b = b;
+
+	division.max_difference = create_bignum_by_power(-((int) digits));
+
+	narrow_iterative(&bounds, &division);
+
+	bignum_cleanup(division.max_difference);
+
+	bignum_cleanup(bounds.lower);
+
+	crop_to_precision(bounds.upper, digits);
+	return bounds.upper;
 }
 
 bignum_t* bignum_divide(const bignum_t* a, const bignum_t* b, unsigned int digits) {
