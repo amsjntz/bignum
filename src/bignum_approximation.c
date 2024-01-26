83399f709c1d555c9139a75ba0aa124d7805e68d
Amos <amsja.mull@gmail.com>
2024-01-26 23:41:42 +0100

    streamlined approximation

---
+++ src/bignum_approximation.c
@@ -0,0 +1,50 @@
+#include "bignum_approximation.h"
+#include "bignum_helpers.h"
+
+bignum_approx_t approx_initialize(bignum_t* lower, bignum_t* upper,
+		unsigned int precision) {
+	return (bignum_approx_t) {
+		.lower = bignum_copy(lower),
+		.upper = bignum_copy(upper),
+		.precision = precision,
+		.max_difference = bignum_create_by_power(-((int) precision))
+	};
+}
+
+bool approx_precise_enough(bignum_approx_t* approx) {
+	if (approx->upper->whole_digits != approx->lower->whole_digits) {
+		return false;
+	}
+
+	bignum_t* difference = bignum_subtract(approx->upper, approx->lower);
+	bignum_comparison_t cmp = bignum_unsigned_compare(difference,
+			approx->max_difference);
+	bignum_cleanup(difference);
+	return cmp != BIGNUM_GT;
+}
+
+bignum_t* approx_create_next_guess(bignum_approx_t* approx) {
+	bignum_t* sum = bignum_add(approx->lower, approx->upper);
+	bignum_t* center = bignum_multiply(sum, BIGNUM_HALF);
+	bignum_cleanup(sum);
+	return center;
+}
+
+void approx_adjust_bounds(bignum_approx_t* approx, bignum_t* guess,
+		bignum_comparison_t direction) {
+	if (direction == BIGNUM_LT) {
+		bignum_cleanup(approx->upper);
+		approx->upper = guess;
+	} else {
+		bignum_cleanup(approx->lower);
+		approx->lower = guess;
+	}
+}
+
+bignum_t* approx_finalize(bignum_approx_t* approx) {
+	bignum_cleanup(approx->max_difference);
+	bignum_cleanup(approx->lower);
+
+	crop_to_precision(approx->upper, approx->precision);
+	return approx->upper;
+}
---
+++ src/bignum_approximation.h
@@ -0,0 +1,35 @@
+#include "../include/bignum.h"
+#include "bignum_helpers.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+	bignum_t* lower;
+	bignum_t* upper;
+
+	unsigned int precision;
+	bignum_t* max_difference;
+} bignum_approx_t;
+
+// I made the decision to not have this be allocated on the heap.
+// After all, the struct is not that large and this function will
+// not be called too often. The performance difference is probably
+// negligable anyways.
+bignum_approx_t approx_initialize(bignum_t* lower, bignum_t* upper,
+		unsigned int precision);
+
+// checks if difference between bounds is <= max_difference
+bool approx_precise_enough(bignum_approx_t* approx);
+
+bignum_t* approx_create_next_guess(bignum_approx_t* approx);
+
+// if direction is BIGNUM_LT, the result will be between lower and guess.
+// if it is BIGNUM_GT, result will be between guess and upper
+void approx_adjust_bounds(bignum_approx_t* approx, bignum_t* guess,
+		bignum_comparison_t direction);
+
+// returns the final approximation and cleans up.
+// does not free the approx pointer, as it's supposed to be on the stack.
+// see comment further up.
+bignum_t* approx_finalize(bignum_approx_t* approx);
